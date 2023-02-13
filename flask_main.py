5483706afbb7f0b53269410d0161b84e0fdbf39a
JowettC <57623021+JowettC@users.noreply.github.com>
2023-02-13 17:08:08 +0800

    flask api

---
+++ flask_main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import pickle 
+pickled_model = pickle.load(open('deploymentWorkshopModel.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return {"message": "Hello World"}
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        numbers = request.get_json()['X']
+    except:
+        return "Error, no input received"
+    
+    # try:
+    #     # return a numpy.ndarray object
+    #     res = pickled_model.predict(numbers)
+    #     # convert into list then take the first element
+    #     return {"res": res.tolist()[0]}
+    # except:
+    #     return {"res": "error"}
+            # return a numpy.ndarray object
+    res = pickled_model.predict([numbers])
+    # convert into list then take the first element
+    return {"res": res.tolist()[0]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
